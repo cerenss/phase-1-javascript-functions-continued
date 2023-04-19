f1085267a7b35b7b25006a394c15994768185c65
Ceren Sahin <cerennssahinn@gmail.com>
2023-04-19 16:34:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    console.log(`This Saturday I want to ${activity}!`);
+}
+
+saturdayFun();
+
+function mondayWork(toDo = "go to the office") {
+    console.log(`On Monday I have to do ${toDo}...`);
+}
+
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+    return function (adj = "special") {
+        return "You are " + flair + adj + flair;
+    }
+}
+
+console.log(wrapAdjective("!")("magnificent beast"));
